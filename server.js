a06f28e8410b5d362c4b477e67609f698d163019
zakaahm <60776348+zakaahm@users.noreply.github.com>
2021-07-18 04:15:29 +0200

    quranSite

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/quran-site'));
+app.use(express.static('./dist/quranSite'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/quran-site/'}),
+    res.sendFile('index.html', {root: 'dist/quranSite/'}),
 );
 
 // Start the app by listening on the default Heroku port
